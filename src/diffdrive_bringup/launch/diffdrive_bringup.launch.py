189645ab951a8299ec73cc9508bebd4c2d58b8e3
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-06-01 11:46:14 +0200

    Change gazebo igni to gazebo harmonic in the launch

--- src/diffdrive_bringup/launch/diffdrive_bringup.launch.py
+++ src/diffdrive_bringup/launch/diffdrive_bringup.launch.py
@@ -24,8 +24,8 @@ def generate_launch_description():
     ]
 
     gazebo = IncludeLaunchDescription(
-        os.path.join(get_package_share_directory("ros_ign_gazebo"), 'launch', 'ign_gazebo.launch.py'),
-         launch_arguments=[('ign_args', [LaunchConfiguration('world'),' -v 4'])]
+        os.path.join(get_package_share_directory("ros_gz_sim"), "launch", "gz_sim.launch.py"),
+        launch_arguments=[("gz_args", ["-r -v 4 ", LaunchConfiguration("world")])],
     )
 
     ign_bridge = Node(
