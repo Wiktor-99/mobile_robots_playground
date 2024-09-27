2830226d8c5505633914ceb22ca331b05c514852
Wiktor Bajor <69388767+Wiktor-99@users.noreply.github.com>
2024-09-27 19:29:53 +0200

    Add tracks conntroller (#2)
    * Add tracks conntroller
    
    * Add service for handling rear tracks control
    
    * Fix minor bugs
    
    * Change impl file name and fill package.xml

---
+++ src/tracked_vehicle/tracks_controller/tracks_controller/tracks_controller.py
@@ -0,0 +1,49 @@
+import rclpy.publisher
+from tracked_vehicle_interfaces.srv import ChainedTracksCommand
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float64
+
+class TracksController(Node):
+    def __init__(self):
+        super().__init__('tracks_controller')
+        self.front_track_control_publishers = [
+            self.create_publisher(Float64, 'front_right_flipper_cmd', 10),
+            self.create_publisher(Float64, 'front_left_flipper_cmd', 10)
+        ]
+        self.rear_track_control_publishers = [
+            self.create_publisher(Float64, 'rear_right_flipper_cmd', 10),
+            self.create_publisher(Float64, 'rear_left_flipper_cmd', 10)
+        ]
+
+        self.front_tracks_control_service = self.create_service(
+            ChainedTracksCommand,
+            'front_tracks_controller',
+            lambda request, response:
+                self.tracks_control_callback(request, response, self.front_track_control_publishers))
+        self.rear_tracks_control_service = self.create_service(
+            ChainedTracksCommand,
+            'rear_tracks_controller',
+            lambda request, response:
+                self.tracks_control_callback(request, response, self.rear_track_control_publishers))
+
+    def tracks_control_callback(
+            self,
+            request: ChainedTracksCommand.Request,
+            response: ChainedTracksCommand.Response,
+            publishers: list[rclpy.publisher.Publisher]):
+        for publisher in publishers:
+            publisher.publish(Float64(data=request.data))
+
+        response.result = True
+        return response
+
+
+def main():
+    rclpy.init()
+    service = TracksController()
+    rclpy.spin(service)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
