024e84a93a6172746479bd1b8f5420bae06a71a8
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-09-27 17:14:31 +0000

    Add service for handling rear tracks control

--- src/tracked_vehicle/tracks_controller/tracks_controller/TracksController.py
+++ src/tracked_vehicle/tracks_controller/tracks_controller/TracksController.py
@@ -1,3 +1,4 @@
+import rclpy.publisher
 from tracked_vehicle_interfaces.srv import ChainedTracksCommand
 import rclpy
 from rclpy.node import Node
@@ -6,16 +7,34 @@ from std_msgs.msg import Float64
 class TracksController(Node):
     def __init__(self):
         super().__init__('')
-        self.front_tracks_control_service = \
-            self.create_service(ChainedTracksCommand, 'front_tracks_controller', self.front_tracks_control_callback)
-        self.front_right_track_control_publisher = self.create_publisher(Float64, 'front_right_flipper_cmd', 10)
-        self.front_left_track_control_publisher = self.create_publisher(Float64, 'front_left_flipper_cmd', 10)
+        self.front_track_control_publishers = [
+            self.create_publisher(Float64, 'front_right_flipper_cmd', 10),
+            self.create_publisher(Float64, 'front_left_flipper_cmd', 10)
+        ]
+        self.rear_track_control_publishers = [
+            self.create_publisher(Float64, 'rear_right_flipper_cmd', 10),
+            self.create_publisher(Float64, 'rear_left_flipper_cmd', 10)
+        ]
 
+        self.front_tracks_control_service = self.create_service(
+            ChainedTracksCommand,
+            'front_tracks_controller',
+            lambda request, response:
+                self.tracks_control_callback(request, response, self.front_track_control_publishers))
+        self.rear_tracks_control_service = self.create_service(
+            ChainedTracksCommand,
+            'rear_tracks_controller',
+            lambda request, response:
+                self.tracks_control_callback(request, response, self.front_track_control_publishers))
 
-    def front_tracks_control_callback(self, request: ChainedTracksCommand.Request, response: ChainedTracksCommand.Response):
+    def tracks_control_callback(
+            self,
+            request: ChainedTracksCommand.Request,
+            response: ChainedTracksCommand.Response,
+            publishers: list[rclpy.publisher.Publisher]):
         data: Float64 = request.data
-        self.front_right_track_control_publisher.publish(data)
-        self.front_left_track_control_publisher.publish(data)
+        for publisher in publishers:
+            publisher.publish(data)
 
         response.result = True
         return response
