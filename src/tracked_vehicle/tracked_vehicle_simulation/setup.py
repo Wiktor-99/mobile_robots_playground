5cdc81b7c5dde855f6559792fded08c049c24751
Wiktor Bajor <wiktorbajor1@gmail.com>
2024-09-28 09:00:46 +0000

    Rename vehicle pkg name

--- src/tracked_vehicle/tracked_vehicle_simulation/setup.py
+++ src/tracked_vehicle/tracked_vehicle_simulation/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 import os
 from glob import glob
 
-package_name = 'tracked_vehicle_simualtion'
+package_name = 'tracked_vehicle_simulation'
 
 setup(
     name=package_name,
